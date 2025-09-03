8a0f53c087aa89b593657a5481ed4da6cf8e7573
omarabdelaziz <3omarabdelaziz123@gmail.com>
2025-09-03 13:06:46 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+dotenv.config({ path: "config.env" });
+
+// Connect with db
+mongoose
+  .connect(process.env.DB_URI)
+  .then((conn) => {
+    console.log(`Database Connected: ${conn.connection.host}`);
+  })
+  .catch((err) => {
+    console.error(`Database Error: ${err}`);
+    process.exit(1);
+  });
+
+const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+  console.log(`mode: ${process.env.NODE_ENV}`);
+}
+
+app.get("/", (req, res) => {
+  res.send("Our Api V7");
+  console.log(res.writableLength);
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
